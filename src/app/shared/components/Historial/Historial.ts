194dba99389309c1e0888a13c92cae824819861e
Daniel Rivas <193283886+D4nielRiv4s@users.noreply.github.com>
2026-03-08 22:41:19 +0100

    Diseñar boton de vista de historia de busqueda

---
+++ src/app/shared/components/Historial/Historial.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'btn-historial',
+  imports: [],
+  templateUrl: './Historial.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class Historial { }
--- src/app/store-front/components/front-navbar/front-navbar.ts
+++ src/app/store-front/components/front-navbar/front-navbar.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Buscar } from '../../../shared/components/buscar/buscar';
 import { SearchService } from '../../../television/services/search.service';
+import { Historial } from "../../../shared/components/Historial/Historial";
 
 @Component({
   selector: 'front-navbar',
-  imports: [RouterLink, RouterLinkActive, Buscar],
+  imports: [RouterLink, RouterLinkActive, Buscar, Historial],
   templateUrl: './front-navbar.html',
 })
 export class FrontNavbar {
